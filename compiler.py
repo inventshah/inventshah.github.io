8b775fc38442fa56025aa816b8ab01dad4242ab3
inventshah <pmshahs2013@gmail.com>
2022-09-27 02:16:05 -0400

    update project list & switch format

--- compiler.py
+++ compiler.py
@@ -4,8 +4,7 @@ December 2021
 
 Compiler script for personal website
 """
-from collections import defaultdict
-from typing import Dict, List, NamedTuple, Optional, Tuple
+from typing import Dict, List, NamedTuple
 import json
 
 from bs4 import BeautifulSoup  # type: ignore
@@ -15,57 +14,50 @@ class Entry(NamedTuple):
     name: str
     desc: str
     links: List[Dict[str, str]]
-    category: str
-    image: Optional[str] = None
+    image: str
     meta: str = ""
 
     def prefix(self) -> str:
-        return "" if not self.meta else f"[{self.meta}] "
+        return f"[{self.meta}] " if self.meta else ""
 
+    def to_html(self) -> str:
+        link = '[<a href="{link}" target="_blank">{label}</a>]'
 
-def project_entry(entry: Entry) -> Tuple[str, str]:
-    link = '[<a href="{link}" target="_blank">{label}</a>]'
+        links = "\n".join(
+            link.format(link=link_info["link"], label=link_info["label"].lower())
+            for link_info in self.links
+        )
 
-    links = "\n".join(
-        link.format(link=link_info["link"], label=link_info["label"].lower())
-        for link_info in entry.links
-    )
-
-    return entry.category, """
-    <li class="entry">
-        {image}
-        <div>
-            <strong>{name}</strong>
-            <br/>
-            {desc}
-            {links}
-        </div>
-    </li>
-    <br/>
-    """.format(
-        name=entry.prefix() + entry.name,
-        desc=entry.desc,
-        links=links,
-        image=f"<img class='icon' src={entry.image!r}/>" if entry.image else "",
-    )
+        return """
+        <li class="entry">
+            {image}
+            <div>
+                <strong>{name}</strong>
+                <br/>
+                {desc}
+                {links}
+            </div>
+        </li>
+        
+        """.format(
+            name=self.prefix() + self.name,
+            desc=self.desc,
+            links=links,
+            image=f"<img class='icon' src={self.image!r}/>",
+        )
 
 
 with open("header.html", "r") as f:
     header = BeautifulSoup(f, features="html.parser")
 
-
 with open("projects.json") as f:
-    data = json.load(f)
-
-data = (Entry(**entry) for entry in data)
-data = map(project_entry, data)
-
-categories = defaultdict(str)
-for category, html in data:
-    categories[category] += html
+    data: Dict[str, List[dict]] = json.load(f)
 
 html = "<hr>".join(
-    (f"<h3>{category}</h3>{content}" for category, content in categories.items())
+    (
+        f'<h3 style="color: #1d73db">{category}</h3>{"".join(Entry(**entry).to_html() for entry in raw_entries)}'
+        for category, raw_entries in data.items()
+    )
 )
 
 html = BeautifulSoup(html, features="html.parser")
